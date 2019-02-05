6d3659ea4b0904f1a011af9d3f11cc9e8e6838ad
B3v3 <boksa.dawid@interia.pl>
2019-02-05 03:23:28 +0100

    add login requirment for product actions(not index and show)and updated tests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :find_product, except: [:index, :new, :create]
+  before_action :find_product,    except: [:index, :new, :create]
+  before_action :authenticate_user!, except:   [:index, :show]
 
   def index
     @products = Product.all
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    email { 'dejw@email.com' }
+    password { 'rekinludojad' }
+    password_confirmation { 'rekinludojad' }
   end
 end
